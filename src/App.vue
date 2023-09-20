34f8448e68049a4dc9e6f4aebea237bc68b68f34
andreyskobliakov <a.skobliakov@gmail.com>
2023-09-21 01:07:18 +0300

    add castom imput

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="bg-gray-100 min-h-screen flex flex-col justify-center items-center px-4 py-8 md:px-10 md:py-8">
     <div class="mb-6 flex flex-wrap gap-3">
-    <CustomInput v-model="selectedTheme" placeholder="Enter theme name" defaultPlaceholder="Default Input"
-      @change-theme="onSetTheme" :class="{
-        'bg-blue-200 text-blue-800 border border-blue-300 rounded p-2': selectedTheme === 'default',
-        'bg-red-200 text-red-800 border border-red-300 rounded p-2': selectedTheme === 'secondary'
-      }"
-    />
-  </div>
+      <CustomInput v-model="selectedTheme" placeholder="Enter theme name" defaultPlaceholder="Default Input"
+        @change-theme="onSetTheme"
+        @input="updateDefaultInputValue"
+      />
+    </div>
+    <div class="mb-6">
+      <p>Name Theme: {{ selectedTheme }}</p>
+    </div>
+    <div class="mb-6 flex flex-wrap gap-3">
+      <input v-model="defaultInputValue"
+        @input="updateCustomInputValue"
+      >
+    </div>
+    <div class="mb-6">
+      <button @click="updateDefaultInputValue">Rebot default input</button>
+    </div>
     <div class="mb-6 flex flex-wrap gap-3">
       <button
         v-for="theme in themes"
@@ -23,7 +32,7 @@
     </div>
     <div class="flex flex-col gap-5 items-center" :class="themeClasses">
       <div v-for="(items, index) in baseColors" :key="index + selectedTheme" class="flex flex-wrap gap-8">
-        <color-card v-for="item in items"
+        <ColorCard v-for="item in items"
           :card-classes="item.value"
           :label="item.label"
           :hideDescription="item.hideDescription"
@@ -43,10 +52,25 @@ import { ref, computed } from "vue";
 
 const themes = ['default', 'secondary'];
 const selectedTheme = ref('default');
+const defaultInputValue = ref('');
 
 const themeClasses = computed(() => `theme-${selectedTheme.value}`);
 
 const onSetTheme = (newTheme) => {
   selectedTheme.value = newTheme;
 }
+
+const updateDefaultInputValue = () => {
+  
+  defaultInputValue.value = selectedTheme.value;
+}
+
+const updateCustomInputValue = () => {
+  
+  selectedTheme.value = defaultInputValue.value;
+}
+
 </script>
+
+<style scoped>
+</style>
