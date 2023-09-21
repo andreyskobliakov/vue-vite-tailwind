ee2f743db7fa6b9c6cb46d06869c4975925a53ed
andreyskobliakov <a.skobliakov@gmail.com>
2023-09-22 01:29:00 +0300

    Wrapper input

---
+++ src/components/Wrapper/CustomInput.vue
@@ -0,0 +1,41 @@
+<template>
+    <InputWrapper :label="label" :error="passwordError">
+      <input v-model="inputValue" @blur="handleBlur" @input="handleInput" :type="type" />
+    </InputWrapper>
+  </template>
+  
+  <script>
+  import InputWrapper from './InputWrapper.vue';
+  
+  export default {
+    components: {
+      InputWrapper,
+    },
+    props: {
+      label: String,
+      type: String,
+    },
+    data() {
+      return {
+        inputValue: '',
+        hasBlurred: false,
+      };
+    },
+    computed: {
+      passwordError() {
+        return this.type === 'password' && this.hasBlurred && this.inputValue.trim() === ''
+          ? 'Password is required'
+          : '';
+      },
+    },
+    methods: {
+      handleInput() {
+        this.inputValue = event.target.value;
+      },
+      handleBlur() {
+        this.hasBlurred = true;
+      },
+    },
+  };
+  </script>
+  
